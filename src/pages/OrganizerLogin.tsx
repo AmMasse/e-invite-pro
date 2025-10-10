6f7a629822abe4248994f93fbe6b267aa656e7c3
Amanyire Daniel <amanyire.daniel1@gmail.com>
2025-10-10 18:58:56 +0300

    Update OrganizerLogin.tsx

--- src/pages/OrganizerLogin.tsx
+++ src/pages/OrganizerLogin.tsx
@@ -58,14 +58,14 @@ const OrganizerLogin = () => {
             <div>
               <CardTitle className="text-2xl">Organizer Login</CardTitle>
               <CardDescription>
-                Enter your Organizer Name and password to access your dashboard
+                Enter your Event Name and password to access your dashboard
               </CardDescription>
             </div>
           </CardHeader>
           <CardContent>
             <form onSubmit={handleLogin} className="space-y-4">
               <div className="space-y-2">
-                <Label htmlFor="eventName">Organizer Name</Label>
+                <Label htmlFor="eventName">Event Name</Label>
                 <Input
                   id="eventName"
                   type="text"
